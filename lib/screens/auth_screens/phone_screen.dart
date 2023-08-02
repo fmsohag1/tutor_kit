3e0862dc730c12f0bfd4b4008de61265b89cdc13
HP <fmsohag34@gmail.com>
2023-08-02 17:50:50 +0600

    test

--- lib/screens/auth_screens/phone_screen.dart
+++ lib/screens/auth_screens/phone_screen.dart
@@ -1,24 +1,18 @@
-import 'dart:async';
-import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tutor_kit/const/consts.dart';
 import 'package:tutor_kit/screens/auth_screens/otp_screen.dart';
-import 'package:tutor_kit/screens/controller/user_controller.dart';
 import 'package:tutor_kit/widgets/custom_button.dart';
 import 'package:tutor_kit/widgets/custom_textfield.dart';
 
 class PhoneScreen extends StatefulWidget {
   const PhoneScreen({super.key, });
-
   @override
   State<PhoneScreen> createState() => _PhoneScreenState();
 }
-
 class _PhoneScreenState extends State<PhoneScreen> {
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgColor,
@@ -39,9 +33,7 @@ class _PhoneScreenState extends State<PhoneScreen> {
               SizedBox(height: 10,),
               CustomButton(onPress: (){
                 Get.to(()=>OtpScreen());
-
               }, text: txtSubmit, color: buttonColor)
-
             ],
           ),
         ),
