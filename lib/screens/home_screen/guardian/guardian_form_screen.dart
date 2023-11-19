4ba3d13bc91dcef21bdc749b0cb3eb89f2a3b098
Shah Arif <shaharifabdullah@gmail.com>
2023-11-20 00:27:26 +0600

    almost ready app bd info

--- lib/screens/home_screen/guardian/guardian_form_screen.dart
+++ lib/screens/home_screen/guardian/guardian_form_screen.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -11,10 +14,122 @@ import 'package:tutor_kit/screens/home_screen/guardian/guardian_home.dart';
 import 'package:tutor_kit/widgets/custom_button.dart';
 import 'package:tutor_kit/widgets/custom_textfield.dart';
 
-class GuardianFormScreen extends StatelessWidget {
+class GuardianFormScreen extends StatefulWidget {
   GuardianFormScreen({super.key});
 
+  @override
+  State<GuardianFormScreen> createState() => _GuardianFormScreenState();
+}
+class Division {
+  int id;
+  String name;
+
+  Division({required this.id, required this.name});
+}
+
+class District {
+  int id;
+  int parentId;
+  String name;
+
+  District({required this.id, required this.parentId, required this.name});
+}
+
+class Upazila {
+  int id;
+  int parentId;
+  String name;
+
+  Upazila({required this.id, required this.parentId, required this.name});
+}
+
+class Union {
+  int id;
+  int parentId;
+  String name;
+
+  Union({required this.id, required this.parentId, required this.name});
+}
+
+class _GuardianFormScreenState extends State<GuardianFormScreen> {
   final _formKey = GlobalKey<FormState>();
+
+
+  String? getSelectedDivisionName() {
+    if (selectedDivisionId != null) {
+      return divisions.firstWhere((division) => division.id == selectedDivisionId).name;
+    }
+    return null;
+  }
+
+  String? getSelectedDistrictName() {
+    if (selectedDistrictId != null) {
+      return filteredDistricts.firstWhere((district) => district.id == selectedDistrictId).name;
+    }
+    return null;
+  }
+
+  String? getSelectedUpazilaName() {
+    if (selectedUpazilaId != null) {
+      return filteredUpazilas.firstWhere((upazila) => upazila.id == selectedUpazilaId).name;
+    }
+    return null;
+  }
+
+  String? getSelectedUnionName() {
+    if (selectedUnionId != null) {
+      return filteredUnions.firstWhere((union) => union.id == selectedUnionId).name;
+    }
+    return null;
+  }
+
+  List<Division> divisions = [];
+  List<District> districts = [];
+  List<Upazila> upazilas = [];
+  List<Union> unions = [];
+
+  List<District> filteredDistricts = [];
+  List<Upazila> filteredUpazilas = [];
+  List<Union> filteredUnions = [];
+
+  int? selectedDivisionId;
+  int? selectedDistrictId;
+  int? selectedUpazilaId;
+  int? selectedUnionId;
+
+
+  @override
+  void initState() {
+    super.initState();
+    // Load data from local JSON files
+    loadData();
+  }
+
+
+  Future<void> loadData() async {
+    final divisionData = await rootBundle.loadString('assets/bd_info/divisions.json');
+    final districtData = await rootBundle.loadString('assets/bd_info/districts.json');
+    final upazilaData = await rootBundle.loadString('assets/bd_info/upazilas.json');
+    final unionData = await rootBundle.loadString('assets/bd_info/unions.json');
+
+    final List<dynamic> divisionList = json.decode(divisionData);
+    final List<dynamic> districtList = json.decode(districtData);
+    final List<dynamic> upazilaList = json.decode(upazilaData);
+    final List<dynamic> unionList = json.decode(unionData);
+
+    divisions = divisionList.map((item) => Division(id: int.parse(item['id']), name: item['name'])).toList();
+    districts = districtList.map((item) => District(id: int.parse(item['id']), parentId: int.parse(item['division_id']), name: item['name'])).toList();
+    upazilas = upazilaList.map((item) => Upazila(id: int.parse(item['id']), parentId: int.parse(item['district_id']), name: item['name'])).toList();
+    unions = unionList.map((item) => Union(id: int.parse(item['id']), parentId: int.parse(item['upazilla_id']), name: item['name'])).toList();
+
+    // Set the initial values to null
+    selectedDivisionId = null;
+    selectedDistrictId = null;
+    selectedUpazilaId = null;
+    selectedUnionId = null;
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,6 +186,87 @@ class GuardianFormScreen extends StatelessWidget {
                     },
                     autoValidate: AutovalidateMode.onUserInteraction,
                     autofillHints: [AutofillHints.telephoneNumberNational],
+                  ),
+                  SizedBox(height: 10,),
+                  DropdownButtonFormField<int?>(
+                    decoration: InputDecoration(
+                        hintText: "Select Division"
+                    ),
+                    value: selectedDivisionId,
+                    items: divisions.map((division) {
+                      return DropdownMenuItem<int?>(
+                        value: division.id,
+                        child: Text(division.name),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedDivisionId = value;
+                        filteredDistricts = districts.where((district) => district.parentId == value).toList();
+                        selectedDistrictId = null;
+                        filteredUpazilas = [];
+                        selectedUpazilaId = null;
+                        filteredUnions = [];
+                        selectedUnionId = null;
+                      });
+                    },
+                  ),
+                  DropdownButtonFormField<int?>(
+                    decoration: InputDecoration(
+                        hintText: "Select District"
+                    ),
+                    value: selectedDistrictId,
+                    items: filteredDistricts.map((district) {
+                      return DropdownMenuItem<int?>(
+                        value: district.id,
+                        child: Text(district.name),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedDistrictId = value;
+                        filteredUpazilas = upazilas.where((upazila) => upazila.parentId == value).toList();
+                        selectedUpazilaId = null;
+                        filteredUnions = [];
+                        selectedUnionId = null;
+                      });
+                    },
+                  ),
+                  DropdownButtonFormField<int?>(
+                    decoration: InputDecoration(
+                        hintText: "Select Upazila"
+                    ),
+                    value: selectedUpazilaId,
+                    items: filteredUpazilas.map((upazila) {
+                      return DropdownMenuItem<int?>(
+                        value: upazila.id,
+                        child: Text(upazila.name),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedUpazilaId = value;
+                        filteredUnions = unions.where((union) => union.parentId == value).toList();
+                        selectedUnionId = null;
+                      });
+                    },
+                  ),
+                  DropdownButtonFormField<int?>(
+                    decoration: InputDecoration(
+                        hintText: "Select Union"
+                    ),
+                    value: selectedUnionId,
+                    items: filteredUnions.map((union) {
+                      return DropdownMenuItem<int?>(
+                        value: union.id,
+                        child: Text(union.name),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedUnionId = value;
+                      });
+                    },
                   ),
                   SizedBox(height: 10,),
                   CustomTextField(
--- lib/screens/home_screen/teacher/teacher_form_screens.dart
+++ lib/screens/home_screen/teacher/teacher_form_screens.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:get/get_connect/http/src/utils/utils.dart';
@@ -15,6 +18,37 @@ class TeacherFormScreen extends StatefulWidget {
 
   @override
   State<TeacherFormScreen> createState() => _TeacherFormScreenState();
+}
+
+class Division {
+  int id;
+  String name;
+
+  Division({required this.id, required this.name});
+}
+
+class District {
+  int id;
+  int parentId;
+  String name;
+
+  District({required this.id, required this.parentId, required this.name});
+}
+
+class Upazila {
+  int id;
+  int parentId;
+  String name;
+
+  Upazila({required this.id, required this.parentId, required this.name});
+}
+
+class Union {
+  int id;
+  int parentId;
+  String name;
+
+  Union({required this.id, required this.parentId, required this.name});
 }
 
 class _TeacherFormScreenState extends State<TeacherFormScreen> {
@@ -48,6 +82,80 @@ class _TeacherFormScreenState extends State<TeacherFormScreen> {
         isDate = true;
       });
     });
+  }
+
+  String? getSelectedDivisionName() {
+    if (selectedDivisionId != null) {
+      return divisions.firstWhere((division) => division.id == selectedDivisionId).name;
+    }
+    return null;
+  }
+
+  String? getSelectedDistrictName() {
+    if (selectedDistrictId != null) {
+      return filteredDistricts.firstWhere((district) => district.id == selectedDistrictId).name;
+    }
+    return null;
+  }
+
+  String? getSelectedUpazilaName() {
+    if (selectedUpazilaId != null) {
+      return filteredUpazilas.firstWhere((upazila) => upazila.id == selectedUpazilaId).name;
+    }
+    return null;
+  }
+
+  String? getSelectedUnionName() {
+    if (selectedUnionId != null) {
+      return filteredUnions.firstWhere((union) => union.id == selectedUnionId).name;
+    }
+    return null;
+  }
+
+  List<Division> divisions = [];
+  List<District> districts = [];
+  List<Upazila> upazilas = [];
+  List<Union> unions = [];
+
+  List<District> filteredDistricts = [];
+  List<Upazila> filteredUpazilas = [];
+  List<Union> filteredUnions = [];
+
+  int? selectedDivisionId;
+  int? selectedDistrictId;
+  int? selectedUpazilaId;
+  int? selectedUnionId;
+
+  @override
+  void initState() {
+    super.initState();
+    // Load data from local JSON files
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    final divisionData = await rootBundle.loadString('assets/bd_info/divisions.json');
+    final districtData = await rootBundle.loadString('assets/bd_info/districts.json');
+    final upazilaData = await rootBundle.loadString('assets/bd_info/upazilas.json');
+    final unionData = await rootBundle.loadString('assets/bd_info/unions.json');
+
+    final List<dynamic> divisionList = json.decode(divisionData);
+    final List<dynamic> districtList = json.decode(districtData);
+    final List<dynamic> upazilaList = json.decode(upazilaData);
+    final List<dynamic> unionList = json.decode(unionData);
+
+    divisions = divisionList.map((item) => Division(id: int.parse(item['id']), name: item['name'])).toList();
+    districts = districtList.map((item) => District(id: int.parse(item['id']), parentId: int.parse(item['division_id']), name: item['name'])).toList();
+    upazilas = upazilaList.map((item) => Upazila(id: int.parse(item['id']), parentId: int.parse(item['district_id']), name: item['name'])).toList();
+    unions = unionList.map((item) => Union(id: int.parse(item['id']), parentId: int.parse(item['upazilla_id']), name: item['name'])).toList();
+
+    // Set the initial values to null
+    selectedDivisionId = null;
+    selectedDistrictId = null;
+    selectedUpazilaId = null;
+    selectedUnionId = null;
+
+    setState(() {});
   }
 
   @override
@@ -164,6 +272,89 @@ class _TeacherFormScreenState extends State<TeacherFormScreen> {
                               ),
                             ],
                           )),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    DropdownButtonFormField<int?>(
+                      decoration: InputDecoration(
+                          hintText: "Select Division"
+                      ),
+                      value: selectedDivisionId,
+                      items: divisions.map((division) {
+                        return DropdownMenuItem<int?>(
+                          value: division.id,
+                          child: Text(division.name),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedDivisionId = value;
+                          filteredDistricts = districts.where((district) => district.parentId == value).toList();
+                          selectedDistrictId = null;
+                          filteredUpazilas = [];
+                          selectedUpazilaId = null;
+                          filteredUnions = [];
+                          selectedUnionId = null;
+                        });
+                      },
+                    ),
+                    DropdownButtonFormField<int?>(
+                      decoration: InputDecoration(
+                          hintText: "Select District"
+                      ),
+                      value: selectedDistrictId,
+                      items: filteredDistricts.map((district) {
+                        return DropdownMenuItem<int?>(
+                          value: district.id,
+                          child: Text(district.name),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedDistrictId = value;
+                          filteredUpazilas = upazilas.where((upazila) => upazila.parentId == value).toList();
+                          selectedUpazilaId = null;
+                          filteredUnions = [];
+                          selectedUnionId = null;
+                        });
+                      },
+                    ),
+                    DropdownButtonFormField<int?>(
+                      decoration: InputDecoration(
+                          hintText: "Select Upazila"
+                      ),
+                      value: selectedUpazilaId,
+                      items: filteredUpazilas.map((upazila) {
+                        return DropdownMenuItem<int?>(
+                          value: upazila.id,
+                          child: Text(upazila.name),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedUpazilaId = value;
+                          filteredUnions = unions.where((union) => union.parentId == value).toList();
+                          selectedUnionId = null;
+                        });
+                      },
+                    ),
+                    DropdownButtonFormField<int?>(
+                      decoration: InputDecoration(
+                          hintText: "Select Union"
+                      ),
+                      value: selectedUnionId,
+                      items: filteredUnions.map((union) {
+                        return DropdownMenuItem<int?>(
+                          value: union.id,
+                          child: Text(union.name),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedUnionId = value;
+                        });
+                      },
                     ),
                     SizedBox(
                       height: 5,
